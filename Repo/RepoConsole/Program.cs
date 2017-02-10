b0e93c8f227aae6f74a4426f71f8d474f7f565ca
Ulrich <backes@solthor.de>
2017-02-10 17:40:02 +0100

    First commit

---
+++ Repo/RepoConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repo;
+using RepoImplementationAsFile;
+
+
+namespace RepoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IRepo repo = new RepoFile();
+
+                Console.WriteLine("Write some words which should be stored");
+                string text = Console.ReadLine();
+
+                repo.Save(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occurs: " + e.Message);
+            }
+        }
+    }
+
+}
---
+++ Repo/RepoFile/RepoFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repo;
+using System.IO;
+
+namespace RepoImplementationAsFile 
+{
+    public class RepoFile : IRepo
+    {
+        public void Save(string text)
+        {
+            File.WriteAllText(@".\RepoFile.txt", text);
+
+            //Alternative for string-array
+            //string[] lines = { "First line", "Second line", "Third line" };
+            //File.WriteAllLines(@".\RepoFile.txt", lines);
+
+            //Alternative with stream
+            //using (StreamWriter file = new StreamWriter(@".\RepoFile.txt"))
+            //{
+            //    file.Write(text);
+            //}
+
+
+            //https://msdn.microsoft.com/de-de/library/8bh11f1k.aspx
+
+
+        }
+    }
+}
